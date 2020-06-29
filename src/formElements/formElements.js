0ba50d1ae54b87c6853dc35e33bce375998601ad
gipertorroid <gipertorroid@gmail.com>
2020-06-29 12:59:17 +0300

    Initial checkboxButtons

--- src/formElements/formElements.js
+++ src/formElements/formElements.js
@@ -1,3 +1,4 @@
 import '../headersFooters/commonBlocks/commonLogoCircle/commonLogoCircle.js';
 import './commonBlocks/dropdown/dropdown.js';
+import './commonBlocks/checkboxButtons/checkboxButtons.js';
 import './formElements.scss';
