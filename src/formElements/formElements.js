d84977092252c4a3de49f8ad228a6b16ec6fbdcd
gipertorroid <gipertorroid@gmail.com>
2020-07-11 18:10:25 +0300

    Making radioButtons

--- src/formElements/formElements.js
+++ src/formElements/formElements.js
@@ -1,4 +1,5 @@
 import '../headersFooters/commonBlocks/commonLogoCircle/commonLogoCircle.js';
 import './commonBlocks/dropdown/dropdown.js';
 import './commonBlocks/checkboxButtons/checkboxButtons.js';
+import './commonBlocks/radioButtons/radioButtons.js';
 import './formElements.scss';
