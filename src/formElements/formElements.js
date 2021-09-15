d62c0021503226f8aab8ae62454d6e607065264e
AndreiKurilov <gipertorroid@gmail.com>
2021-09-15 16:07:10 +0300

    feat(formElements): add textField

--- src/formElements/formElements.js
+++ src/formElements/formElements.js
@@ -1,5 +1,6 @@
 import '../headersFooters/commonBlocks/commonLogoCircle/commonLogoCircle.js';
 import './commonBlocks/dropdown/dropdown.js';
+import './commonBlocks/textField/textField.js';
 import './commonBlocks/checkboxButtons/checkboxButtons.js';
 import './commonBlocks/radioButtons/radioButtons.js';
 import './commonBlocks/toggle/toggle.js';
