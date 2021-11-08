5a0d50aaa16fc1318d11f4c5467726ca58c93d49
AndreiKurilov <gipertorroid@gmail.com>
2021-11-08 12:09:41 +0300

    refactor(setColorsType): change the file structure

--- src/setColorsType/setColorsType.js
+++ src/setColorsType/setColorsType.js
@@ -1,3 +1,5 @@
 import '../headersFooters/commonBlocks/commonLogoCircle/commonLogoCircle.js';
 import '../commonFonts/fonts.scss';
 import './setColorsType.scss'
+import './colors/colors.js'
+import './types/types.js'
