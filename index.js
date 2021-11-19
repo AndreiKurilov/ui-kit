ab652192dc1a0f8bcccf321107d8b7e1f7294b45
AndreiKurilov <gipertorroid@gmail.com>
2021-11-19 12:42:19 +0300

    refactor(App): change the file structure

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ import './src/setColorsType/setColorsType.js';
 import './src/cards/cards.js';
 import './src/headersFooters/headersFooters.js';
 import './src/formElements/formElements.js';
+import './src/fonts/fonts.scss';
 import './index.scss';
