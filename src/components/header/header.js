3562326dc83ae96a9a6e1afd02ef0fa12bc2b270
AndreiKurilov <gipertorroid@gmail.com>
2021-11-26 07:21:11 +0300

    refactor(header): change the file structure

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,5 +1,4 @@
 import '../logo/logo.js';
 import '../nav/nav.js';
-import '../buttonLogin/buttonLogin.js';
-import '../buttonRegister/buttonRegister.js';
+import '../button/button.js';
 import './header.scss';
