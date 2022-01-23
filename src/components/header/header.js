5bcd758dcadbf146daf845f92dddc4d57a84c9ee
AndreiKurilov <gipertorroid@gmail.com>
2022-01-23 11:30:40 +0300

    refactor(header): change structure of file

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,3 +2,14 @@ import '../logo/logo.js';
 import '../nav/nav.js';
 import '../button/button.js';
 import './header.scss';
+
+const menuBurgerItem = document.querySelectorAll(".js-menu-burger");
+if( menuBurgerItem.length > 0) {
+  menuBurgerItem.forEach((item) => {
+    item.addEventListener ('click', function(){
+      document.querySelector('.header__menu-burger').classList.toggle('active');
+      document.querySelector('.header__menu-burger-icon').classList.toggle('active');
+      document.querySelector('.header__wrapper').classList.toggle('active');
+    })
+  })
+}
