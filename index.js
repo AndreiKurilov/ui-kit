e5a10e7fcb8d4f015efb1bdc0288f6a163f5f086
AndreiKurilov <gipertorroid@gmail.com>
2022-02-07 11:55:29 +0300

    refactor(App): change structure of file

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ import './src/cards/cards.js';
 import './src/headersFooters/headersFooters.js';
 import './src/formElements/formElements.js';
 import './src/fonts/fonts.scss';
+import './src/components/logo/logo.js';
 import './index.scss';
