4477fdf0c4fd3c361be6b38e25e02a94b90e9c08
AndreiKurilov <gipertorroid@gmail.com>
2022-03-19 11:21:52 +0300

    refactor(dropdown): change structure of file

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -1,1 +1,24 @@
+import '../dropdownItem/dropdownItem.js';
 import './dropdown.scss';
+
+class ExpandableDropdown {
+  constructor(selector) {
+    this.selector = selector;
+    this.addListener();
+  }
+  
+  addListener() {
+    this.selector.addEventListener('click', () =>
+    this.selector.classList.toggle('active'));
+  }
+}
+
+const dropdownExpanded = document.querySelectorAll('.dropdown__block');
+
+if (dropdownExpanded.length > 0) {
+  dropdownExpanded.forEach((selector) => new ExpandableDropdown(selector));
+}
+
+let itemsTotal = document.querySelector('.dropdown__total');
+import {numberDefault} from "../dropdownItem/dropdownItem.js";
+itemsTotal.innerText = `${numberDefault} спальни`;
