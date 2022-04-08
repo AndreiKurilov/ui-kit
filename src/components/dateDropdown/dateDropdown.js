29b2ad854c606f1f420fec856add5954b6fa6446
AndreiKurilov <gipertorroid@gmail.com>
2022-04-08 12:52:23 +0300

    feat(dateDropdown): create dateDropdown

--- src/components/dateDropdown/dateDropdown.js
+++ src/components/dateDropdown/dateDropdown.js
@@ -11,7 +11,7 @@ const dateDropdown = new AirDatepicker('.textField__input', {
 const textFieldInput = document.querySelector('.textField__input');
 
 textFieldInput.addEventListener('keydown', function(event) {
-		if ((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
-			event.preventDefault();
-		}
+  if ((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
+    event.preventDefault();
+  }
 });
