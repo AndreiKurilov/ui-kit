67b378ec653f335c2524b756ae5c71adcdbb4bc6
AndreiKurilov <gipertorroid@gmail.com>
2022-04-30 16:00:35 +0300

    refactor(App): delete useless file

--- index.js
+++
@@ -1,7 +0,0 @@
-import './src/setColorsType/setColorsType.js';
-import './src/cards/cards.js';
-import './src/headersFooters/headersFooters.js';
-import './src/formElements/formElements.js';
-import './src/fonts/fonts.scss';
-import './src/components/logo/logo.js';
-import './index.scss';
