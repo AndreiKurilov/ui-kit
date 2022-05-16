04a49444c24ceb1e9b7bf9978c56def4c49744f3
AndreiKurilov <gipertorroid@gmail.com>
2022-05-16 11:38:39 +0300

    feat(dropdown): add state.js

---
+++ src/components/dropdown/state.js
@@ -0,0 +1,12 @@
+export const optionsComfort = {
+  bedrooms: ["спальня", "спальни"],
+  beds: ["кровать", "кровати"],
+  bathrooms: ["ванная комната", "ванные комнаты"],
+  placeholder: "Удобства номера",
+}
+
+export const optionsGuests = {
+  guests: ["гость", "гостя", "гостей"],
+  babies: ["младенец", "младенца"],
+  placeholder: "Сколько гостей",
+}
