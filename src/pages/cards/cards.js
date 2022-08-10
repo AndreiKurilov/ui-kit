18b39064358ff15642ef703a286c86118d17c83d
AndreiKurilov <gipertorroid@gmail.com>
2022-08-10 11:28:16 +0300

    refactor(cards): change file structure

--- src/pages/cards/cards.js
+++ src/pages/cards/cards.js
@@ -1,2 +1,32 @@
-import '../../layouts/layout.scss';
+import '../../components/cardRegistration/cardRegistration.js';
+import '../../components/cardSearchRoom/cardSearchRoom.js';
+import '../../components/dateDropdown/dateDropdown.js';
+import '../../components/roomWidget/roomWidget.js';
 import './cards.scss'
+
+import AirDatepicker from 'air-datepicker';
+import 'air-datepicker/air-datepicker.css';
+import '../../components/dateDropdown/dateDropdown.scss';
+
+let buttonApply = {
+  content: 'Применить',
+  className: 'air-datepicker-button',
+  onClick: (dp) => {
+    dp.hide();
+  }
+}
+
+const dateDropdownInline = new AirDatepicker(".cards__airDatepicker", {
+  position: 'bottom center',
+  classes: "air-datepicker",
+  classes: "air-datepicker_big",
+  prevHtml: '<span class="air-datepicker__arrow air-datepicker__arrow_back"></span>',
+  nextHtml: '<span class="air-datepicker__arrow air-datepicker__arrow_forward"></span>',
+  navTitles: {
+    days: 'MMMM yyyy'
+  },
+  buttons: [ 'clear', buttonApply ],
+  range: true,
+  multipleDates: 2,
+  selectedDates: [ '2019-08-19', '2019-08-23' ],
+})
