d1123df94c6a990629ffa14d8b36112a544798ff
AndreiKurilov <gipertorroid@gmail.com>
2022-08-17 12:19:19 +0300

    refactor(headersFooters): change file structure

--- src/pages/headersFooters/headersFooters.js
+++ src/pages/headersFooters/headersFooters.js
@@ -1,6 +1,4 @@
 import '../../components/header/header.js';
 import '../../components/footer/footer.js';
 import '../../components/footerMobile/footerMobile.js';
-import '../../fonts/fonts.scss';
-import '../../layouts/layout.scss';
 import './headersFooters.scss';
