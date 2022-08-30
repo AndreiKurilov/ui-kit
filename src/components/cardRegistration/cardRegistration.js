f1141555aa9f786086c7196a42e3f88f1ef31aaa
AndreiKurilov <gipertorroid@gmail.com>
2022-08-30 10:15:44 +0300

    refactor(cardRegistration): change file structure

--- src/components/cardRegistration/cardRegistration.js
+++ src/components/cardRegistration/cardRegistration.js
@@ -1,1 +1,7 @@
+import '../textField/textField.js';
+import '../radio/radio.js';
+import '../heading/heading.js';
+import '../toggle/toggle.js';
+import '../button/button.js';
+import '../../fonts/fonts.scss';
 import './cardRegistration.scss';
