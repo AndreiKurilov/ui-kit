bea1397ac685d769d36a4b6ef823154b198d06f9
AndreiKurilov <gipertorroid@gmail.com>
2022-09-08 10:37:02 +0300

    refactor(setColorsType): add import path

--- src/pages/setColorsType/setColorsType.js
+++ src/pages/setColorsType/setColorsType.js
@@ -1,4 +1,6 @@
 import './colors/colors.js'
 import './types/types.js'
 import '../../fonts/fonts.scss';
+import '../../components/linksPages/linksPages.js';
+import '../../layouts/layout.scss';
 import './setColorsType.scss'
