fc224963b919b823b317ab4556d2de19f26d3462
AndreiKurilov <gipertorroid@gmail.com>
2022-09-16 12:11:59 +0300

    feat(landingPage): create landingPage

---
+++ src/pages/landingPage/landingPage.js
@@ -0,0 +1,6 @@
+import '../../components/header/header.js';
+import '../../components/footer/footer.js';
+import '../../components/cardSearchRoom/cardSearchRoom.js';
+import '../../fonts/fonts.scss';
+import '../../layouts/layout.scss';
+import './landingPage.scss';
