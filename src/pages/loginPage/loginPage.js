08489849631ce9fda4068e2570d01dbf578294ca
AndreiKurilov <gipertorroid@gmail.com>
2022-09-24 20:19:19 +0300

    feat(loginPage): create loginPage

---
+++ src/pages/loginPage/loginPage.js
@@ -0,0 +1,6 @@
+import '../../components/header/header.js';
+import '../../components/footer/footer.js';
+import '../../components/cardRegistration/cardRegistration.js';
+import '../../fonts/fonts.scss';
+import '../../layouts/layout.scss';
+import './loginPage.scss';
